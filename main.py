cfb8c5b07c4dfdda93f5d3507a88dff913791aaf
WenchangLu <wlu2@ncsu.edu>
2022-04-22 22:44:11 -0700

    add an example file

--- main.py
+++ main.py
@@ -6,17 +6,22 @@ import subprocess
 from cif2rmg import *
 st.title('RMG input User Interface')
 cif_file = st.file_uploader("upload cif file", type=".cif")
+example_ =  st.checkbox("use an example cif file FeAs.cif", False)
 if cif_file:
   if not os.path.isdir("tempDir"):
     os.mkdir("tempDir")
   with open(os.path.join("tempDir", cif_file.name), "wb") as f:
     f.write(cif_file.getbuffer())
   filename = "tempDir/"+cif_file.name
+elif example_:
+  filename = "cifs/FeAs.cif"  
 
+if cif_file or example_:
   crmg = cifrmg_interface()
   rmginput_str = crmg.cif2rmg_run(filename)
+  #st.write(crmg.species)
 
-  rmgfilename = cif_file.name.split(".")[0] +".rmg"
+  rmgfilename = os.path.basename(filename).split(".")[0] +".rmg"
   st.download_button(
      label="Downlowd rmg input file",
      data=rmginput_str,
